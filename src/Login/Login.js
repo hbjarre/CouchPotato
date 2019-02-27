61679d039ff3a61900cab3c75e1b199eca8c8e37
mrich <mrich@kth.se>
2019-02-27 11:11:06 +0100

    Update Login.js

---
+++ src/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <p>Login sidan wohoo!</p>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
