c8163e1907f109c095c013507bff8279e8e55ef5
mrich <mrich@kth.se>
2019-02-27 11:06:50 +0100

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Welcome from "./Welcome/Welcome";
-import Header from "./Header/Header";
 import Login from "./Login/Login";
 import "./App.css";
 
--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "../Header/Header";
 import "./Welcome.css";
 
 class Welcome extends Component {
