13af35bffe9a18e854e40f08386951bb0bfb415e
mrich <mrich@kth.se>
2019-03-01 11:03:19 +0100

    Create Settings.js

---
+++ src/Settings/Settings.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Welcome.css";
+import fire from "../config/Fire";
+
+class Settings extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>The settings page!</p>
+      </div>
+    );
+  }
+}
+
+export default Settings;
