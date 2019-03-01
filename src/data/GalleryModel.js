26e6c587a9293f7710f84ca12c08e05a03bb4bac
Oliver Shin <oliver.shin.swe@gmail.com>
2019-03-01 10:58:13 +0100

    model and gitignore

---
+++ src/data/GalleryModel.js
@@ -0,0 +1,42 @@
+import ObservableModel from "./ObservableModel";
+
+
+const APIKEY = "bad7ef2d"
+const BASE_URL = "http://www.omdbapi.com/?apikey="+APIKEY+"&";
+
+
+class GalleryModel extends ObservableModel {
+
+    constructor() {
+        super();
+        
+      }
+
+      getMovie(search_string) {
+        //http://www.omdbapi.com/?apikey=bad7ef2d&t=Captain+Marvel
+        //http://www.omdbapi.com/?t=Captain+Marvel
+        const url = `${BASE_URL}t=` + search_string;
+        return fetch(url).then(this.processResponse);
+      }
+
+      getTest() {
+
+        const url = `${BASE_URL}t=Avatar`;
+        console.log(url)
+        var movie = fetch(url).then(this.processResponse);
+        console.log(movie.results)
+        return movie
+
+      }
+      
+      processResponse(response) {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      }
+}
+
+// Export an instance of DinnerModel
+const modelInstance = new GalleryModel();
+export default modelInstance;
---
+++ src/data/apikey.js
@@ -0,0 +1,1 @@
+const API_KEY = "bad7ef2d";
