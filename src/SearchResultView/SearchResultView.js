9afa532addc298018c94fb4ab37d73a985ce6e91
skihpper <naiikewa@gmail.com>
2019-03-29 15:55:06 +0100

    Better looking text

--- src/SearchResultView/SearchResultView.js
+++ src/SearchResultView/SearchResultView.js
@@ -111,7 +111,7 @@ class SearchResults extends Component {
         var next_btn = <button onClick={this.NextPage}>next</button>
         if (movies.results != undefined) {
           html = movies.results.map((element, index) =>
-            <Link to={`/movie/${element.id}`} key={index}><MovieCard movie={element} /></Link>
+            <Link to={`/movie/${element.id}`} style={{ textDecoration: "black", color: "black" }} key={index}><MovieCard movie={element} /></Link>
           );
         }
 
