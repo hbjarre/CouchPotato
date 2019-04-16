2799ce65546fa75bf10917d11b716b0ef9e9a8ec
Oliver Shin <oliver.shin.swe@gmail.com>
2019-04-16 14:59:03 +0200

    new genres

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -75,10 +75,12 @@ discoverGenre (genre) {
       <div className="container">
       <div className="d-flex">
       <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.updateMovies()}}>Popular movies</button></div>
-      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Science Fiction")}}>Discover SCI-FI</button></div>
-      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Comedy")}}>Discover Comedy</button></div>
-      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Horror")}}>Discover Horror</button></div>
-      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Documentary")}}>Discover Documentary</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Action")}}>Action</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Drama")}}>Drama</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Comedy")}}>Comedy</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Science Fiction")}}>SCI-FI</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Horror")}}>Horror</button></div>
+      <div><button type="button" className="btn btn-outline-light wht" onClick={() => {this.discoverGenre("Documentary")}}>Documentary</button></div>
         </div>
         <div className="card-container">{html}</div>
       </div>
@@ -87,3 +89,22 @@ discoverGenre (genre) {
 }
 
 export default Welcome;
+/*0: {id: 28, name: "Action"}
+1: {id: 12, name: "Adventure"}
+2: {id: 16, name: "Animation"}
+3: {id: 35, name: "Comedy"}
+4: {id: 80, name: "Crime"}
+5: {id: 99, name: "Documentary"}
+6: {id: 18, name: "Drama"}
+7: {id: 10751, name: "Family"}
+8: {id: 14, name: "Fantasy"}
+9: {id: 36, name: "History"}
+10: {id: 27, name: "Horror"}
+11: {id: 10402, name: "Music"}
+12: {id: 9648, name: "Mystery"}
+13: {id: 10749, name: "Romance"}
+14: {id: 878, name: "Science Fiction"}
+15: {id: 10770, name: "TV Movie"}
+16: {id: 53, name: "Thriller"}
+17: {id: 10752, name: "War"}
+18: {id: 37, name: "Western"}*/
